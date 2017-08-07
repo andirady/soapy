e5935d0f64575d48cce15575540a8102d9df347e
Andi Rady Kurniawan <andirady@kendra-systems.com>
2017-08-07 10:19:47 +0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+import os
+from setuptools import setup
+
+setup(
+    name='soapy',
+    version='0.9.0',
+    description='A simple SOAP service mocker',
+    url='https://github.com/andirady/soapy',
+    author='Andi Rady Kurniawan',
+    license='MIT',
+    keywords='soap server mockup'
+)
